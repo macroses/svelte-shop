483e4c502dbb7103a2a823a98b945cef26cf5e5f
Konstantin Bashkov <kostyabashkov@gmail.com>
2021-08-11 17:34:34 +0300

    model

---
+++ src/model/data.js
@@ -0,0 +1,15 @@
+class MainData {
+    constructor(name, price, description, img) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.img = img;
+    }
+
+    addElement() {}
+    getElement() {}
+    updateElement() {}
+    removeElement() {}
+}
+
+export default MainData;
