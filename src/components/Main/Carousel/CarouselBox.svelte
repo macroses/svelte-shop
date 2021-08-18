4a99ff3a1065d08929d30cec059f0df2f015ad8c
Лщтыефтешт <>
2021-08-18 22:12:46 +0300

    carousel

---
+++ src/components/Main/Carousel/CarouselBox.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Carousel from 'svelte-carousel';
+	import Model from '../../../model/data-service';
+
+	const temp = new Model();
+
+	const allimg = temp.getAllItems()
+</script>
+
+<Carousel
+    autoplay
+    autoplayDuration={5000}
+    autoplayProgressVisible
+>
+</Carousel>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,13 @@
 <script>
     import CategoryList from "../components/Main/CategoryList.svelte";
+    import CarouselBox from "../components/Main/Carousel/CarouselBox.svelte";
 </script>
 
 <main>
     <div class="container">
         <div class="main_content">
             <CategoryList/>
+            <CarouselBox/>
         </div>
     </div>
 </main>
