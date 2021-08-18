9b6d34d5056759d33cd307573231da72e334a9ec
Лщтыефтешт <>
2021-08-18 23:29:16 +0300

    get all data

--- src/components/Main/Carousel/CarouselBox.svelte
+++ src/components/Main/Carousel/CarouselBox.svelte
@@ -1,15 +1,24 @@
 <script>
-    import Carousel from 'svelte-carousel';
 	import Model from '../../../model/data-service';
 
 	const temp = new Model();
 
 	const allimg = temp.getAllItems()
+
+    allimg.then(val => console.log(val[0].category))
 </script>
 
-<Carousel
-    autoplay
-    autoplayDuration={5000}
-    autoplayProgressVisible
->
-</Carousel>
+
+{#await allimg then value}
+    {#each value[0].category as item (item.id)}
+        <p>{item.id}</p>
+        <p>{item.brand}</p>
+        <p>{item.title}</p>
+        {item.body}
+        {#each item.imgSet as elem}
+            <img src="{elem}" alt="">
+        {/each}
+
+    {/each}
+
+{/await}
