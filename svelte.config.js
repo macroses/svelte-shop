ebdf5c6dd5e6c963344222585d1f25d6541d9176
Konstantin <>
2021-10-23 13:10:12 +0300

    "begin" deploy settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
+import begin from '@architect/sveltekit-adapter';
+
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter()
+		adapter: begin()
 	}
 };
 
