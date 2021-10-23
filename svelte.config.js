84ed859205ac273cbc93123bdea607c371095323
Konstantin <>
2021-10-23 13:37:15 +0300

    delete begin settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
+import adapter from '@sveltejs/adapter-netlify';
+
 /** @type {import('@sveltejs/kit').Config} */
-import begin from '@architect/sveltekit-adapter';
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: begin()
+		adapter: adapter(),
 	}
 };
 
