336c16207392fbc12585258e700448f03b8b738a
Konstantin <>
2021-10-23 14:30:08 +0300

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
-
+import vercel from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter()
+		adapter: vercel()
 	}
 };
 
